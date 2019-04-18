b64f7ea328e3452946a259dc4815fb437424ffcd
Sulaymon333 <tjsulaymon@gmail.com>
2019-04-18 03:12:40 +0300

    code refactoring and fixed additional bugs

--- validate.js
+++ validate.js
@@ -1,29 +1,43 @@
 /*=== GET DOM elements ===*/
-
 const inputFields = document.querySelectorAll('input');
 const form = document.querySelector('#form');
 
+/*=== input patterns ===*/
+const patterns = {
+  firstname: { pattern: /^[a-zA-Z]{2,12}$/ },
+  lastname: { pattern: /^[a-zA-Z]{2,12}$/ },
+  username: { pattern: /^[\w\-]{2,12}$/ },
+  email: { pattern: /^[\w\.]{6,12}\@[a-z]{2,6}\.[a-z]{2,6}(\.[a-z]{2,6})?$/ },
+  password: { pattern: /^[\w-@]{6,32}$/ },
+  telephone: { pattern: /^[\d]{3}(\ |\-)[\d]{3}(\ |\-)[\d]{4}$/ },
+  bio: { pattern: /^[\w-\ \.]{3,60}$/ }
+};
+
 /*=== Event listeners ===*/
-const arr = [];
+let arr = [];
 inputFields.forEach(input => {
   input.addEventListener('input', e => {
-    if (fieldsData[input.id].pattern.test(e.target.value)) {
+    if (patterns[input.id].pattern.test(e.target.value)) {
       input.className = 'valid';
-      // fieldsData[input.id].status = true;
       arr.push(input.id);
     } else {
       input.className = 'invalid';
-      // fieldsData[input.id].status = false;
     }
     let uniqueArr = new Set(arr);
-    console.log(Array.from(uniqueArr));
+    arr = Array.from(uniqueArr);
 
-    if (Array.from(uniqueArr).length === inputFields.length) {
+    if (arr.length === inputFields.length) {
       form['submit-button'].disabled = false;
       form['submit-button'].style.backgroundColor = 'green';
     }
-    e.preventDefault();
   });
 });
 
-form.addEventListener('submit', e => form.reset());
+form.addEventListener('submit', e => {
+  form.reset();
+  arr = [];
+  form['submit-button'].disabled = true;
+  form['submit-button'].style.backgroundColor = 'transparent';
+  inputFields.forEach(input => input.classList.remove('valid'));
+  e.preventDefault();
+});
